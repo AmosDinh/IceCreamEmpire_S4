ce03e2a9fb470a9607e957427236106455375871
Jannik Voelker <jannik.voelker@sap.com>
2023-05-16 13:46:07 +0200

    added some nice queries

---
+++ src/frontend/app/classes/queries.sql
@@ -0,0 +1,24 @@
+-- Retrieve the total sales revenue for each ice cream flavor:
+SELECT F.name AS flavor_name,
+    SUM(OD.amount * OD.price * (1 - OD.discount)) AS total_revenue
+FROM Flavors F
+    INNER JOIN OrderDetails OD ON F.flavor_id = OD.flavor_id
+GROUP BY F.name;
+--Find the best-selling ice cream flavors based on the total quantity sold:
+SELECT F.name AS flavor_name,
+    SUM(OD.amount) AS total_quantity_sold
+FROM Flavors F
+    INNER JOIN OrderDetails OD ON F.flavor_id = OD.flavor_id
+GROUP BY F.name
+ORDER BY total_quantity_sold DESC
+LIMIT 5;
+--Identify the neighborhoods with the highest average order amount
+SELECT N.name AS neighborhood_name,
+    AVG(OD.amount * OD.price * (1 - OD.discount)) AS average_order_amount_discounted
+FROM Neighborhoods N
+    INNER JOIN Tours T ON N.Neighborhood_id = T.Neighborhood_id
+    INNER JOIN Orders O ON T.tours_id = O.tours_id
+    INNER JOIN OrderDetails OD ON O.order_id = OD.order_id
+GROUP BY N.name
+ORDER BY average_order_amount_discounted DESC
+LIMIT 3;
